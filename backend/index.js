61c10c8633cbb11defd39236978a027ef3685ca9
sakshisrivastava1 <sakshisrivas33@gmail.com>
2026-01-28 15:42:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://vybe-frontend-08a9.onrender.com',
   credentials: true   
 }))
 
